4e52d26767ee292d5643579ada79e8df71f18b19
Javier Ruiz <javiersebastian2005@gmail.com>
2025-07-15 20:53:35 -0400

    Apparently added Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -40,7 +43,9 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler.ksp)
     implementation(libs.androidx.core.ktx)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
